aaca5e52ed5d412fbad853587459044aef15f69f
root <phashateboho@gmail.com>
2024-04-19 08:11:02 -0700

    intsall a program using puppet, as well as executing a command

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,12 @@
+# Puppet manifest to install Flask version 2.1.0
+
+package { 'python3-pip':
+  ensure => installed,
+}
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  path    => ['/usr/bin', '/usr/local/bin'],
+  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
+}
+

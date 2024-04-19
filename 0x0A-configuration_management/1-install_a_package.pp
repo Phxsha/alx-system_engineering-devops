bae414b68768feeb643e4d74669cfa4f07a726a9
root <phashateboho@gmail.com>
2024-04-19 08:36:04 -0700

    mod path

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -5,8 +5,7 @@ package { 'python3-pip':
 }
 
 exec { 'install_flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
+  command => 'pip3 install Flask==2.1.0',
   path    => ['/usr/bin', '/usr/local/bin'],
   unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
 }
-

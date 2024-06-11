c3a44ed6419297f2baf04d3b3708ea2f5861b6c1
root <phashateboho@gmail.com>
2024-06-11 07:40:06 -0700

    task 0 update

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,54 +1,6 @@
-# Increases the limits on my system and Nginx so large requests are processed
+# fix nginx to accept and serve more requests
 
-exec { 'increase_system_limits':
-  command => 'echo -e "* soft nofile 65535\n* hard nofile 65535\n" >> /etc/security/limits.conf && echo "fs.file-max = 100000" >> /etc/sysctl.conf && sysctl -p',
-  unless  => 'grep -q "fs.file-max = 100000" /etc/sysctl.conf',
-}
-
-file { '/etc/nginx/nginx.conf':
-  ensure  => file,
-  content => template('nginx/nginx.conf.erb'),
-  notify  => Service['nginx'],
-}
-
-file { '/etc/nginx/nginx.conf.erb':
-  ensure  => file,
-  content => @("EOF"),
-worker_processes auto;
-
-worker_rlimit_nofile 65535;
-
-events {
-    worker_connections 8192;
-}
-
-http {
-    include       /etc/nginx/mime.types;
-    default_type  application/octet-stream;
-
-    sendfile        on;
-    keepalive_timeout  65;
-
-    server {
-        listen       80;
-        server_name  localhost;
-
-        location / {
-            root   html;
-            index  index.html index.htm;
-        }
-
-        error_page   500 502 503 504  /50x.html;
-        location = /50x.html {
-            root   html;
-        }
-    }
-}
-  | EOF
-}
-
-service { 'nginx':
-  ensure    => running,
-  enable    => true,
-  subscribe => File['/etc/nginx/nginx.conf'],
+exec {'modify max open files limit setting':
+  command => 'sed -i "s/15/10000/" /etc/default/nginx && sudo service nginx restart',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
 }
